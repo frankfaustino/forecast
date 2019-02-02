0c1d277b9230a62c350c9d49a3c415892f883907
Frank Faustino <nawzyah@gmail.com>
2019-02-02 02:50:08 -0800

    ✨ Clover OAuth server-side

--- forecasten/server/src/index.ts
+++ forecasten/server/src/index.ts
@@ -1,6 +1,10 @@
 import { ApolloServer } from 'apollo-server-express'
+import axios from 'axios'
+import * as connectRedis from 'connect-redis'
 import { config } from 'dotenv'
 import * as express from 'express'
+import * as session from 'express-session'
+import * as Redis from 'ioredis'
 import 'reflect-metadata'
 import { buildSchema } from 'type-graphql'
 
@@ -8,7 +12,23 @@ import { UserResolver } from './modules/user/UserResolver'
 
 export const main = (async () => {
   config()
-  const { PORT } = process.env
+  const { APP_ID, APP_SECRET, CLIENT_URI, CLOVER_URI, PORT } = process.env
+  const RedisStore = connectRedis(session)
+  const redis = new Redis()
+
+  const sesh = session({
+    cookie: {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7 * 365,
+      secure: process.env.NODE_ENV === 'production'
+    },
+    name: 'qid',
+    resave: false,
+    saveUninitialized: false,
+    secret: 'supersecret',
+    store: new RedisStore({ client: redis as any })
+  })
+
   const app = express()
   const server = new ApolloServer({
     schema: await buildSchema({
@@ -16,7 +36,39 @@ export const main = (async () => {
     })
   })
 
-  server.applyMiddleware({ app })
+  app.use(sesh)
+
+  const requestAPIToken = async (req: any, res: any) => {
+    const url = `${CLOVER_URI}/token?client_id=${APP_ID}&client_secret=${APP_SECRET}&code=${
+      req.query.code
+    }`
+    await axios
+      .get(url)
+      .then(({ data }) => {
+        req.session.access_token = data.access_token
+        req.session.save()
+        console.log('🥳', req.session, req.sessionID)
+        res.redirect(`${CLIENT_URI}/auth_success`)
+      })
+      .catch(err => {
+        console.log('😭️', err)
+        res.send(err)
+      })
+  }
+
+  const authenticate = async (req: any, res: any) => {
+    const url = `${CLOVER_URI}/authorize?client_id=${APP_ID}`
+    req.query.code ? await requestAPIToken(req, res) : await res.redirect(url)
+  }
+
+  app.get('/auth', (req, res) => authenticate(req, res))
+
+  const cors = {
+    credentials: true,
+    origin: '*'
+  }
+
+  server.applyMiddleware({ app, cors })
 
   app.listen(PORT, () => console.log(`🤖 Server is listening on port ${PORT}`))
 })()
