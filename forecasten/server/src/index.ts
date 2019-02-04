08b4426b5e08af85ce8516e4621b727745c468bf
Frank Faustino <nawzyah@gmail.com>
2019-02-03 22:00:16 -0800

    🚧 Server WIP (will wrap REST with GraphQL)

--- forecasten/server/src/index.ts
+++ forecasten/server/src/index.ts
@@ -8,11 +8,12 @@ import * as Redis from 'ioredis'
 import 'reflect-metadata'
 import { buildSchema } from 'type-graphql'
 
-import { UserResolver } from './modules/user/UserResolver'
+import { MerchantResolver } from './modules/user/MerchantResolver'
 
 export const main = (async () => {
   config()
-  const { APP_ID, APP_SECRET, CLIENT_URI, CLOVER_URI, PORT } = process.env
+  const { CLIENT_URI, PORT, PROD_APP_ID, PROD_APP_SECRET, PROD_CLOVER_URI } = process.env
+  // const { APP_ID, APP_SECRET, CLIENT_URI, CLOVER_URI, PORT, PROD_APP_ID, PROD_APP_SECRET, PROD_CLOVER_URI } = process.env
   const RedisStore = connectRedis(session)
   const redis = new Redis()
 
@@ -32,22 +33,29 @@ export const main = (async () => {
   const app = express()
   const server = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [UserResolver]
+      resolvers: [MerchantResolver]
     })
   })
 
   app.use(sesh)
 
   const requestAPIToken = async (req: any, res: any) => {
-    const url = `${CLOVER_URI}/token?client_id=${APP_ID}&client_secret=${APP_SECRET}&code=${
+    const url = `${PROD_CLOVER_URI}/oauth/token?client_id=${PROD_APP_ID}&client_secret=${PROD_APP_SECRET}&code=${
       req.query.code
     }`
+    // const url = `${CLOVER_URI}/oauth/token?client_id=${APP_ID}&client_secret=${APP_SECRET}&code=${
+    //   req.query.code
+    // }`
+    // Save response from Clover OAuth to session
+    Object.assign(req.session, req.query)
+
     await axios
       .get(url)
       .then(({ data }) => {
         req.session.access_token = data.access_token
         req.session.save()
-        console.log('🥳', req.session, req.sessionID)
+        console.log('🥳', data, req.query, req.session, req.sessionID)
+
         res.redirect(`${CLIENT_URI}/auth_success`)
       })
       .catch(err => {
@@ -57,11 +65,20 @@ export const main = (async () => {
   }
 
   const authenticate = async (req: any, res: any) => {
-    const url = `${CLOVER_URI}/authorize?client_id=${APP_ID}`
+    const url = `${PROD_CLOVER_URI}/oauth/authorize?client_id=${PROD_APP_ID}`
     req.query.code ? await requestAPIToken(req, res) : await res.redirect(url)
   }
 
   app.get('/auth', (req, res) => authenticate(req, res))
+
+  app.get('/inventory', async (req, res) => {
+    const { access_token, merchant_id } = req.session as any
+    const url = `${PROD_CLOVER_URI}/v3/merchants/${merchant_id}/items?access_token=${access_token}`
+    const response: any = await axios.get(url).catch((err: any) => console.log('🐛', err.message))
+    if (response && response.data) {
+      res.json(response.data)
+    }
+  })
 
   const cors = {
     credentials: true,
