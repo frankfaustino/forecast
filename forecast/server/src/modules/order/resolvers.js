925aeb8e64a9cd601ac1bd512f5dcea64c103ab4
Frank Faustino <nawzyah@gmail.com>
2019-04-19 19:03:42 -0700

    📝 Add expanded lineItems field

--- forecast/server/src/modules/order/resolvers.js
+++ forecast/server/src/modules/order/resolvers.js
@@ -2,16 +2,15 @@ const axios = require('axios')
 
 const baseUrl = `${process.env.SANDBOX_CLOVER_URI}/v3/merchants`
 
-const merchant = async (parent, args, { req: { session } }, info) => {
+const order = async (parent, args, { req: { session } }, info) => {
   const { access_token, merchant_id } = session
-  const url = `${baseUrl}/${merchant_id}/orders?access_token=${access_token}`
+  const url = `${baseUrl}/${merchant_id}/orders?access_token=${access_token}&expand=lineItems`
   const response = await axios.get(url).catch(err => console.error('🐛', err.message))
 
   if (response && response.data) {
-    console.log('☀️', response)
     return response.data
   }
   return null
 }
 
-module.exports = { merchant }
+module.exports = { order }
